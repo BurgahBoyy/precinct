e0916787686a676c2ca43ea6695e690e911818e9
Robert Vasquez <rob@sailwellness.app>
2026-07-01 22:51:38 -0400

    feat: CSP allows leaflet cdn + OSM tiles

--- precinct/hardening.py
+++ precinct/hardening.py
@@ -11,9 +11,11 @@ import os
 import threading
 import time
 
-CSP = ("default-src 'self'; script-src 'self' 'unsafe-inline'; "
-       "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; "
-       "font-src https://fonts.gstatic.com; img-src 'self' data:; connect-src 'self'")
+CSP = ("default-src 'self'; script-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com; "
+       "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdnjs.cloudflare.com; "
+       "font-src https://fonts.gstatic.com; "
+       "img-src 'self' data: https://tile.openstreetmap.org https://*.tile.openstreetmap.org; "
+       "connect-src 'self'")
 
 HEADERS = {
     "X-Content-Type-Options": "nosniff",
